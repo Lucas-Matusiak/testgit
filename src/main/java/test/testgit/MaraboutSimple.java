37c6e264123e3dde456f901aaf05248d665dc04e
Lucas <lucassmatusiak@gmail.com>
2019-04-06 21:18:27 +0200

    Ajout Marabout simple 3 mots de plus

--- src/main/java/test/testgit/MaraboutSimple.java
+++ src/main/java/test/testgit/MaraboutSimple.java
@@ -3,7 +3,7 @@ package test.testgit;
 public class MaraboutSimple {
 	
     public static String getResult() {
-       return "Marabout, Bout de ficelle, Selle de cheval";
- }
-
+    	return "Marabout, Bout de ficelle, Selle de cheval"
+	              + ", Cheval de course, Course à pied, Pied à terre";
+    }
 }   
